97c46fc2a07560201eb315e3637ba49c141fe094
TranMinn <1801040141@s.hanu.edu.vn>
2020-12-11 00:40:33 +0700

    QTDesign

---
+++ Lab class B/linearRegression.py
@@ -0,0 +1,18 @@
+from __future__ import print_function
+import numpy as np
+import matplotlib.pyplot as plt
+from matplotlib.backends.backend_pdf import PdfPages
+
+
+# height (cm)
+# each row is a point
+X = np.array([[147, 150, 153, 158, 163, 165, 168, 170, 173, 175, 178, 180, 183]]).T
+
+# weight (kg)
+y = np.array([ 49, 50, 51,  54, 58, 59, 60, 62, 63, 64, 66, 67, 68])
+# Visualize data
+plt.plot(X, y, 'ro')
+plt.axis([140, 190, 45, 75])
+plt.xlabel('Height (cm)')
+plt.ylabel('Weight (kg)')
+plt.show()
